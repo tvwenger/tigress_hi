2eb806a2138375d1bebc5aa28a1fbaa4040c0447
Trey Wenger <tvwenger@gmail.com>
2024-09-17 14:41:57 -0500

    update split_data.py

--- scripts/split_data.py
+++ scripts/split_data.py
@@ -10,9 +10,15 @@ for fname in fnames:
     dirname = fname.replace(".pkl", "")
     if not os.path.isdir(dirname):
         os.mkdir(dirname)
-    with open(fname, 'rb') as f:
+    with open(fname, "rb") as f:
         data = pickle.load(f)
-    for idx, row in data.iterrows():
+
+    for idx in range(len(data["data_list"])):
         print(fname, idx)
-        with open(f"{dirname}/{idx:06d}.pickle", 'wb') as f:
-            pickle.dump(row, f)
+
+        datum = {}
+        for key in data.keys():
+            datum[key] = data[key][idx]
+
+        with open(f"{dirname}/{idx:06d}.pkl", "wb") as f:
+            pickle.dump(datum, f)
